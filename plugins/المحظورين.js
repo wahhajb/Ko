457587e7ece208ecff225494b23ce4b37c52f07f
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-06 02:54:13 +0300

    Update المحظورين.js

--- plugins/المحظورين.js
+++ plugins/المحظورين.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, {conn}) => {
   await conn.fetchBlocklist().then(async (data) => {
-    let txt = `*≡ الاشخاص الي واخدين بلوك*\n\n*الرقم :* ${data.length}\n\n┌─⊷\n`;
+    let txt = `*≡ الاشخاص الي اخذو بلوك*\n\n*الرقم :* ${data.length}\n\n┌─⊷\n`;
     for (const i of data) {
       txt += `▢ @${i.split('@')[0]}\n`;
     }
