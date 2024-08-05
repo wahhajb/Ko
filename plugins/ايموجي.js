1d96da7a031bfdd88c8a20875645bdf1a0a12d02
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-06 02:59:54 +0300

    Update ايموجي.js

--- plugins/ايموجي.js
+++ plugins/ايموجي.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 ❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
 ❐↞┇الـجـائـزة💵↞ ${poin} دولار┇
-*『𝐌𝐀𝐃𝐀𝐑𝐀↳🐢↲𝐁𝐎𝐓』*
+*『نسيانن١↳🐢↲𝐁𝐎𝐓』*
 `.trim();
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
